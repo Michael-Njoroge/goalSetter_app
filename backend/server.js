166895ee63054295dce0bfe16584f7e4f8a9b17d
Michael-Njoroge <mikethecoder12@gmail.com>
2023-11-13 20:31:04 +0300

    complete goals route

--- backend/server.js
+++ backend/server.js
@@ -10,18 +10,4 @@ app.listen(port, () => {
     console.log(`Server started on port ${port}`)
 });
 
-app.get('/api/goals', (req,res) => {
-    res.status(200).json({message: 'Get Goals'})
-});
-
-app.post('/api/goals', (req,res) => {
-    res.status(200).json({message: 'Set Goal'})
-});
-
-app.put('/api/goals/:id', (req,res) => {
-    res.status(200).json({message: `Update Goal {:id}`})
-});
-
-app.delete('/api/goals/:id', (req,res) => {
-    res.status(200).json({message: 'Delete Goal'})
-});
+app.use('/api/goals', require('./routes/goalsRoute'));
