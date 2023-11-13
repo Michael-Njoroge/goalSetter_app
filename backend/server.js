45910ad3a865292c167fb0a70d27c189ef2b4474
Michael-Njoroge <mikethecoder12@gmail.com>
2023-11-13 20:16:16 +0300

    start up on CRUD operation

--- backend/server.js
+++ backend/server.js
@@ -9,3 +9,19 @@ const app = express()
 app.listen(port, () => {
     console.log(`Server started on port ${port}`)
 });
+
+app.get('/api/goals', (req,res) => {
+    res.status(200).json({message: 'Get Goals'})
+});
+
+app.post('/api/goals', (req,res) => {
+    res.status(200).json({message: 'Set Goal'})
+});
+
+app.put('/api/goals/:id', (req,res) => {
+    res.status(200).json({message: `Update Goal {:id}`})
+});
+
+app.delete('/api/goals/:id', (req,res) => {
+    res.status(200).json({message: 'Delete Goal'})
+});
